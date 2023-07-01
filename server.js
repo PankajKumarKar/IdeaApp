84b4795e12fcb1af8a99917d6f25fbe3eb263f60
PankajKumarKar <pankajkar14@gmail.com>
2023-07-01 17:39:22 +0530

    Create Server Config file

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const serverConfig = require("./configs/serverConfig");
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server started");
+app.listen(serverConfig.Port, () => {
+  console.log(`Server Runnig At Port No : ${serverConfig.Port}`);
 });
