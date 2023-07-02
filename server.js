f203e592b749f41452ed48c57b194b149dd7997b
PankajKumarKar <pankajkar14@gmail.com>
2023-07-02 16:18:53 +0530

    db Connection

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const serverConfig = require("./configs/serverConfig");
+const mongoose = require("mongoose");
+const dbConfig = require("./configs/db.config");
 
 const app = express();
+
+mongoose.connect(dbConfig.DB_URL);
+
+const db = mongoose.connection;
+
+db.on("error", () => {
+  console.log("Error While Db Connection");
+});
+
+db.once("open", () => {
+  console.log("Db Connected");
+});
 
 app.listen(serverConfig.Port, () => {
   console.log(`Server Runnig At Port No : ${serverConfig.Port}`);
